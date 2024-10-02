a238c27ca65986685f0b6d740e8db8ae3ad83e74
samleticias <sammyaleticia09@gmail.com>
2024-10-01 21:57:25 -0300

    feat: adiciona estrelas para avaliação de filmes

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -3,17 +3,11 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import "./index.scss"
-
-export interface MovieType {
-    id: number,
-    title: string,
-    poster_path: string,
-    overview: string,
-    vote_average: number
-}
+import MovieCard from "../MovieCard";
+import { Movie } from "@/types/movie";
 
 export default function MovieList(){
-    const [movies, setMovies] = useState<MovieType[]>([]);
+    const [movies, setMovies] = useState<Movie[]>([]);
 
     useEffect(() => {
         getMovies();
@@ -34,18 +28,10 @@ export default function MovieList(){
     return (
         <ul className="movie-list">
         {movies.map(movie => 
-            <li key={movie.id} className="movie-card">
-                <p>
-                    {movie.title}
-                </p>
-                <p className="description">
-                    {movie.overview}
-                </p>
-                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt=""/>
-                <p>
-                    {movie.vote_average}
-                </p>
-            </li>
+            <MovieCard 
+                key={movie.id} 
+                movie={movie}
+            />
         )}
         </ul>
     );
